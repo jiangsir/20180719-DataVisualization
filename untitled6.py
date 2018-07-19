ec5b9350eb32264002c0b7682dec0a9f1bb54a95
jiangsir <jiangzero@gmail.com>
2018-07-19 17:26:25 +0800

    資策會-資料視覺化

---
+++ untitled6.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 10:43:05 2018
+
+@author: jiangsir
+"""
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+print(type(df))
